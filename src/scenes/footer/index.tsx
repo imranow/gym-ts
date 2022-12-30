8df3cb8482b1b39711978a50851ce26b0627bf75
Imran Tanbir <tanbirimran@gmail.com>
2022-12-30 12:32:51 +0000

    updated footer

--- src/scenes/footer/index.tsx
+++ src/scenes/footer/index.tsx
@@ -1,7 +1,12 @@
 import Logo from "@/assets/Logo.png";
-type Props = {};
+import { SelectedPage } from "@/shared/types";
 
-const Footer = (props: Props) => {
+
+type Props = {
+  setSelectedPage: (value: SelectedPage) => void;
+};
+
+const Footer = ({setSelectedPage}: Props) => {
   return (
     <footer className="bg-primary-100 py-16">
       <div className="justify-content mx-auto w-5/6 gap-16 md:flex">
